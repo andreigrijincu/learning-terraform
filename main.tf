a9eb00c69e158d5b11b626ed6a5f20d2f52e765e
andreigrijincu <163527399+andreigrijincu@users.noreply.github.com>
2024-03-29 15:27:28 +0200

    adding ssh to sg

--- main.tf
+++ main.tf
@@ -59,6 +59,17 @@ resource "aws_security_group_rule" "web_https_in" {
   security_group_id = aws_security_group.web.id
 }
 
+resource "aws_security_group_rule" "ssh_in" {
+  type          = "ingress"
+  description   = "Allow ssh from anywhere"
+  from_port     = 22
+  to_port       = 22
+  protocol      = tcp
+  cidr_blocks   = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+}
+
 resource "aws_security_group_rule" "web_all_out" {
   type          = "egress"
   from_port     = 0
