79fd29fb909f25b17ee33cf4d4fcb68ef266880f
andreigrijincu <163527399+andreigrijincu@users.noreply.github.com>
2024-03-29 15:39:19 +0200

    test overwrite

--- main.tf
+++ main.tf
@@ -70,6 +70,16 @@ resource "aws_security_group_rule" "ssh_in" {
   security_group_id = aws_security_group.web.id
 }
 
+resource "aws_security_group_rule" "sssh_in" {
+  type          = "ingress"
+  description   = "Allow ssh from anywhere"
+  from_port     = 222
+  to_port       = 222
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+
 resource "aws_security_group_rule" "web_all_out" {
   type          = "egress"
   from_port     = 0
